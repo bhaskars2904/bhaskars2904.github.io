491f80a38c9a4cf1a8595537644ee1f944b2499e
Bhaskar Sharma <bhaskar.sharma@zemosolabs.com>
2017-12-02 22:27:57 +0530

    added service worker and index file

---
+++ HTTPhost/push-worker.js
@@ -0,0 +1,10 @@
+// 86acbd31cd7c09cf30acb66d2fbedc91daa48b86:1511947674.46
+importScripts('https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js')
+uaSetup.worker(self, {
+  defaultIcon: 'https://www.flaticon.com/free\u002Dicon/notifications_565423',
+  defaultTitle: 'NFDemo default title',
+  defaultActionURL: 'https://www.gmail.com/',
+  appKey: 'V5kIBVz2Sty8Rq3oSWps9A',
+  token: 'MTpWNWtJQlZ6MlN0eThScTNvU1dwczlBOjgzQUJzVHRNem5pTm83TGlaaldiNVd6X3d1Q2NrUUh4SXdKMHQ1QmV2ZkE',
+  vapidPublicKey: 'BIuF1XnM48hMVwL7HYyfoBPcdCCiVmCRTgOKqV8LIKUDLZBJJSE7FyuT1j23GGhqmLv_u5xJTwmgpKKpPNQvmGE='
+})
